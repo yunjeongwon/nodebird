9924a2a747d622aadc9722c97fab9eba337f2ddd
jeongwon <yunjwon94@gmail.com>
2021-09-08 14:38:33 +0900

    before lightsail

---
+++ logger.js
@@ -0,0 +1,18 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
+module.exports = logger;
